8994ec53cc18d680389d5aa54b3defabddfafbc7
Abdullah Al Maruf <marufoffice.67@gmail.com>
2024-03-07 02:21:33 +0600

    seeder added

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,5 +18,11 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+        $this->call([
+            CreateUsersSeeder::class,
+            HomepageSeeder::class,
+            CountriesTableSeeder::class,
+        ]);
     }
 }
